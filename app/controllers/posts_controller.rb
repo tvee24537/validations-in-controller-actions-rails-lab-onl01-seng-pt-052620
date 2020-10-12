c2403b7092b7341d56d979c0bdf0971d9d11a18e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 19:08:20 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    @new_post = Post.new(post_params)
+    if @new_post.valid?
+      @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :email, uniqueness: true
 end
